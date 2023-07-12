77fd2e1d35f380a90d62b7d23399cceec4084218
Vakockhaidze <139356496+Vakockhaidze@users.noreply.github.com>
2023-07-12 17:06:00 +0400

    Add files via upload

---
+++ დავალება 3/homework3.py
@@ -0,0 +1,11 @@
+    # name = "Vako"
+    # surname = "Ckhaidze"
+    # age = 14
+    # height = 1.63 
+    # country = "Georgia"
+    # city = "Tbilisi"
+    # print(name + " " + surname + " " + str(age) + " " + str(height) + " " + country + " " + city)
+
+num1 = 20
+num2 = 19
+print(str(num1) + str(num2))
